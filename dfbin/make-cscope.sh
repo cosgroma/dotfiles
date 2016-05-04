79cb69be96a2eac74f803f657f81c0f46be98674
Mathew Cosgrove <mathew.cosgrove@ngc.com>
2016-05-03 17:51:12 -0700

    renamed some shit

--- dfbin/make-cscope.sh
+++ dfbin/make-cscope.sh
@@ -12,12 +12,14 @@ t_csf=$PDIR/temp.cscope.files
 [ -e "$csf" ] && echo "cleaning previous scope files" && rm $csf
 
 find $PDIR -name "*.[chxsS]" -print > $t_csf
-
+echo "making cscope for $PDIR, please wait..."
 while read line; do
     echo $(cygpath -aw $line) >> $csf;
 done < $t_csf
-
+echo "finished processing source files... running cscope"
 pushd .
 cd $PDIR
 cscope -b
 popd
+
+rm $t_csf
