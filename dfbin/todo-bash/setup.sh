364464a0a97d59ab33bde4694e3e95b9ffed715b
Mathew Cosgrove <mathew.cosgrove@ngc.com>
2017-05-11 08:25:43 -0700

    removed submodule and added todo files

---
+++ dfbin/todo-bash/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+################################################################################
+## @file    setup.sh - {{project_name}}
+## @author  Mathew Cosgrove mathew.cosgrove@ngc.com
+## @Date:   2017-05-09 08:06:09
+## @brief
+## @copyright
+## @version
+## @Last Modified by:   Mathew Cosgrove
+## @Last Modified time: 2017-05-09 08:06:41
+#
+## @details
+## @par URL
+##  @n
+#
+## @par Purpose
+#
+##
+## @note
+##
+#
+## @par Usage
+##
+################################################################################
+
+export PATH=~/dfbin/todo-bash:~/dfbin/todo-bash/internals/:$PATH
+export TODOFILE=~/workspace/notes/todo/todo
+export TODOHISTORY=~/workspace/notes/todo/todo-history
+alias t=todo
+alias d=todone
